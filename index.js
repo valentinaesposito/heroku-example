0ab366f9339b38ae927d24b45ce0419d579ac0eb
Pasquale Boemio <boemianrapsodi@gmail.com>
2014-08-30 17:00:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('Hi AT student! :)');
+});
+
+var port = Number(process.env.PORT || 8080);
+var server = app.listen(port, function() {
+  console.log('Listening on port %d', server.address().port);
+});
